f01fc77de14e8fa4fafabc16d130721629a3f77f
puzan <pujan.shrestha@nextbytetech.com>
2018-12-27 21:08:01 +0545

    gitignore update

---
+++ frontend/build/precache-manifest.cf87b53e0445de18ef5adb42aaf03d2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d624dfb3fc8002a1b6d0",
+    "url": "/static/css/main.5b6c0501.chunk.css"
+  },
+  {
+    "revision": "d624dfb3fc8002a1b6d0",
+    "url": "/static/js/main.d624dfb3.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "78b072cd844c8ad2875a3177d44bf99d",
+    "url": "/index.html"
+  }
+];
