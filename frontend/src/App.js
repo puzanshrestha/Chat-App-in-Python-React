25197d12cb6d4dda4fca6bf0a616175ecd5451ac
puzan <pujan.shrestha@nextbytetech.com>
2018-12-27 07:45:59 +0545

    change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,38 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-	componentDidMount(){
-			var loc = window.location
-			var wsStart ="ws://"
-
-
-			if(loc.protocol === "https:"){
-				wsStart ="wss://"
-			}
-
-
-			var endpoint = wsStart+loc.host+loc.pathname+"ws/"
-
-			var socket = new WebSocket(endpoint);
-
-			socket.onmessage = function(e){
-				console.log("message",e);
-				console.log(e.data)
-			}
-			socket.onopen = function(e){
-				console.log("open",e);
-			}
-
-			socket.onclose = function(e){
-				console.log("close",e);
-			}
-
-			socket.onerror = function(e){
-				console.log("error",e);
-			}
-
-			window.socket=socket;
-	}
   render() {
     return (
       <div className="App">
