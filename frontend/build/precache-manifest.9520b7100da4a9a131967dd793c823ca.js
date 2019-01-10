2871417e75e3f507c77cd813e58d9f036e9880b9
puzan <pujan.shrestha@nextbytetech.com>
2019-01-10 11:48:21 +0545

    on logout refresh users list

---
+++ frontend/build/precache-manifest.9520b7100da4a9a131967dd793c823ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26956df83b02ccae3a57",
+    "url": "/static/css/main.8ba0218d.chunk.css"
+  },
+  {
+    "revision": "26956df83b02ccae3a57",
+    "url": "/static/js/main.26956df8.chunk.js"
+  },
+  {
+    "revision": "082a6d3dd16fa6194404",
+    "url": "/static/js/1.082a6d3d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f5ab7b473ef9b0ab07c284765d6cc2ff",
+    "url": "/index.html"
+  }
+];
